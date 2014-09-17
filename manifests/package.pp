ca68d5dfe1dc14442f7b0e7b15442f6fe02504b7
Johann Dickson <johann.dickson@gmail.com>
2014-09-17 14:10:56 +0200

    Added package repository

--- manifests/package.pp
+++ manifests/package.pp
@@ -6,4 +6,26 @@ class prosody::package {
       ;
   }
 
+  file {
+    'apt-source':
+      ensure  => 'present',
+      path    => '/etc/apt/sources.list.d/prosody.list',
+      content => template('prosody/prosody.list.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      ;
+  }
+
+  exec {
+    'apt-key':
+      command  => 'wget https://prosody.im/files/prosody-debian-packages.key -O-\
+        | apt-key --keyring /etc/apt/trusted.gpg.d/prosody.gpg add -',
+      creates  => '/etc/apt/trusted.gpg.d/prosody.gpg',
+      provider => 'shell',
+      require  => File['apt-source'],
+      before   => Package['prosody'],
+      ;
+  }
+
 }
