4fc755e0589abcc136b944df2d54d3e589c88b2b
Johann Dickson <johann.dickson@gmail.com>
2014-09-17 14:51:53 +0200

    Manage server defaults

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,13 @@
 class prosody(
+  $admins                 = [],
+  $daemonize              = true,
+  $pidfile                = '/var/run/prosody/prosody.pid',
+  $modules_enabled        = ['defaults'],
+  $modules_disabled       = [],
+  $authentication         = 'internal_plain',
+  $allow_registration     = false,
+  $c2s_require_encryption = false,
+  $s2s_require_encryption = false,
 ){
 
   class {
